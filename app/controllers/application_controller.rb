bf3cdc6c09f1c23b01573c9d871620bf58f0a5d7
naoya.kawabata <naoya.kawabata@prum.jp>
2023-09-12 13:43:38 +0900

    Renderデータベースリセット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      Rails.logger.info "Username: #{username}, Password: #{password}"
+      username == 'admin' && password == '39424'
+    end
+  end  
 end
