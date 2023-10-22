59cdd029933247554260237a8d7c29d673c2780d
naoya.kawabata <naoya.kawabata@prum.jp>
2023-10-22 22:42:44 +0900

    category_idの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -44,7 +44,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id == @item.user_id || @item.orders.present?
+    if current_user.id == @item.user_id || @item.order.present?
       redirect_to root_path
     end
   end
