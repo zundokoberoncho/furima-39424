964f69b0bef0cf025b6de14e053c30942d841b8f
naoya.kawabata <naoya.kawabata@prum.jp>
2023-10-28 16:50:58 +0900

    商品出品機能の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,23 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(){
-  // console.log("JavaScript file is loaded!");
-
 document.addEventListener("turbo:load", () => {
-  // console.log("Turbo is loaded!");
   const priceInput = document.getElementById("item-price");
   const addTaxDom = document.getElementById("add-tax-price");
   const profitDom = document.getElementById("profit");
-
-  // console.log("priceInput:", priceInput);
-
   if (priceInput && addTaxDom && profitDom) {
     priceInput.addEventListener("input", () => {
       const inputValue = priceInput.value;
-      const addTax = Math.floor(inputValue * 0.1); 
+      const addTax = Math.floor(inputValue * 0.1);
       const profit = Math.floor(inputValue - addTax);
-      
+
       addTaxDom.innerHTML = addTax;
       profitDom.innerHTML = profit;
     });
-    }
-  });
+  }
 });
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "card", to: "card.js"
 pin "item_price", to: "item_price.js"
+pin "card", to: "card.js"
